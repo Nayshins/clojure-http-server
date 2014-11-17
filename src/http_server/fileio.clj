3fdbcf7189b7e604e08efa318b0e4f8c34c7b768
Jake Nations <jnations@8thlight.com>
2014-11-17 10:10:12 -0600

    Rename file interactor to fileio

---
+++ src/http_server/fileio.clj
@@ -0,0 +1,15 @@
+(ns http_server.fileio
+  (:require [clojure.java.io :as io]))
+
+(defn binary-slurp [path]
+  (with-open [out (java.io.ByteArrayOutputStream.)]
+    (io/copy (io/input-stream path) out)
+    (.toByteArray out)))
+
+(defn append-to-file [path body]
+  (spit path body :append true))
+
+(defn overwrite-file [path body]
+  (spit path body))
+
+
