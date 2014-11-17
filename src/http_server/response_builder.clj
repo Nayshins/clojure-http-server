49f1377b921f9a2fe5d5f48ceff8dcad4e6018d9
Jake Nations <jnations@8thlight.com>
2014-11-17 10:09:02 -0600

    Change response builder to use int instead of keyword

--- src/http_server/response_builder.clj
+++ src/http_server/response_builder.clj
@@ -2,18 +2,19 @@
   (:require [clojure.java.io :as io]))
 
 (set! *warn-on-reflection* true)
-(def response-code { :200 "200 OK\r\n"
-                     :204 "204 NO CONTENT\r\n"
-                     :206 "206 PARTIAL CONTENT\r\n"
-                     :301 "301 MOVED PERMANENTLY\r\n"
-                     :401 "401 UNAUTHORIZED\r\n"
-                     :404 "404 NOT FOUND\r\n"
-                     :405 "405 METHOD NOT ALLOWED\r\n"
-                     :409 "409 CONFLICT\r\n"
-                     :500 "500 INTERNAL SERVER ERROR\r\n"})
+(def response-code { 200 " OK\r\n"
+                     204 " NO CONTENT\r\n"
+                     206 " PARTIAL CONTENT\r\n"
+                     301 " MOVED PERMANENTLY\r\n"
+                     400 " BAD REQUEST\r\n"
+                     401 " UNAUTHORIZED\r\n"
+                     404 " NOT FOUND\r\n"
+                     405 " METHOD NOT ALLOWED\r\n"
+                     409 " CONFLICT\r\n"
+                     500 " INTERNAL SERVER ERROR\r\n"})
 
 (defn build-code [code]
-  (byte-array (.getBytes (str "HTTP/1.1 " (response-code code)))))
+  (byte-array (.getBytes (str "HTTP/1.1 " code (response-code code)))))
 
 (defn build-headers [headers]
   (if (not-empty headers)
--- src/http_server/router.clj
+++ src/http_server/router.clj
@@ -1,11 +1,11 @@
 (ns http-server.router
-  (:require [http_server.response-builder :refer :all]
-            [http_server.file-interactor :refer :all]
+  (:require [http_server.response-builder :as response-builder]
+            [http_server.fileio :as fileio]
             [http-server.config :refer :all]
             [clojure.java.io :as io]
             [base64-clj.core :as base64]
             [pantomime.mime :refer [mime-type-of]]
-            [pandect.core :refer :all]))
+            [pandect.core :as pandect]))
 
 (set! *warn-on-reflection* true)
 
@@ -42,7 +42,7 @@
 (defn build-directory [directory]
   (let [directory-links (to-byte-array 
                           (build-directory-links directory))]
-    (build-response :200 
+    (response-builder/build-response 200 
                     {"Content-Length" (count directory-links)} 
                     directory-links)))
 
@@ -67,7 +67,7 @@
         begin (Integer. ^String (first byte-range))
         end (+ 1 (Integer. ^String (second byte-range)))
         body (get-trimmed-body body-bytes begin end)]
-    (build-response :206 {"Content-Type"
+    (response-builder/build-response 206 {"Content-Type"
                           (mime-type-of (io/file path))
                           "Content-Length"
                           (count body)}
@@ -76,23 +76,23 @@
 (defn get-file-data [directory location headers]
   (try
     (let [path (str directory location)
-          body-data (binary-slurp path)]
+          body-data (fileio/binary-slurp path)]
       (if (contains? headers :Range)
         (get-body-range body-data (headers :Range) path)
-        (build-response :200
+        (response-builder/build-response 200
                         {"Content-Type" 
                          (mime-type-of (io/file path))
                          "Content-Length" 
                          (count body-data) } 
                         body-data)))
     (catch Exception e
-      (build-response :404 {}))))
+      (response-builder/build-response 404 {}))))
 
 (defn authenticate [directory location headers]
   (let [no-auth (.getBytes "Authentication required")]
     (if (headers :Authorization) 
       (get-file-data directory location headers) 
-      (build-response :401 {} no-auth))))
+      (response-builder/build-response 401 {} no-auth))))
 
 (defn decode-params [params]
   (let [params (clojure.string/replace params #"=" " = ")]
@@ -103,7 +103,7 @@
 
 (defn handle-query [params]
   (let [decoded-params (decode-params params)]
-    (build-response :200 
+    (response-builder/build-response 200 
                     {:Content-Length (count decoded-params)} 
                     decoded-params)))
 
@@ -119,8 +119,8 @@
     (contain-route? (config-options :accept-parameters) location)
       (handle-query params)
     (contain-route? (config-options :redirect) location) 
-      (build-response :301 {"Location" "http://localhost:5000/"})
-    :else (build-response :500 {})))
+      (response-builder/build-response 301 {"Location" "http://localhost:5000/"})
+    :else (response-builder/build-response 500 {})))
 
 (defn get-route [location directory headers] 
   (let [query (clojure.string/split location #"\?") 
@@ -134,42 +134,42 @@
       :else (get-file-data directory location headers))))
 
 (defn options-route [location directory]
-  (build-response :200 {"Allow" "GET,HEAD,POST,OPTIONS,PUT"}))
+  (response-builder/build-response 200 {"Allow" "GET,HEAD,POST,OPTIONS,PUT"}))
 
 (defn not-valid-file [location]
   (some (partial = location) (config-options :protected)))
 
 (defn patch-route [body location directory headers]
   (cond 
-    (not-valid-file location) (build-response :405 {})
+    (not-valid-file location) (response-builder/build-response 405 {})
     :else
     (let [path (str directory location)
           file-data (slurp path)
-          encoded-file-data (sha1 file-data)
+          encoded-file-data (pandect/sha1 file-data)
           etag (headers :If-Match)]
-      (overwrite-file path body)
-      (build-response :204 {}))))
+      (fileio/overwrite-file path body)
+      (response-builder/build-response 204 {}))))
 
 (defn post-route [body location directory]
   (cond
-    (not-valid-file location) (build-response :405 {})
+    (not-valid-file location) (response-builder/build-response 405 {})
     :else (do 
-            (append-to-file (str directory location) body)
-            (build-response :200 {}))))
+            (fileio/append-to-file (str directory location) body)
+            (response-builder/build-response 200 {}))))
 
 (defn put-route [body location directory]
   (cond
-    (not-valid-file location) (build-response :405 {})
+    (not-valid-file location) (response-builder/build-response 405 {})
     :else (do 
             (spit (str directory location) body)
-            (build-response :200 {}))))
+            (response-builder/build-response 200 {}))))
 
 (defn delete-route [location directory]
   (spit (str directory location) "")
-  (build-response :200 {}))
+  (response-builder/build-response 200 {}))
 
 (defn head-route [location directory]
-  (build-response :200 {}))
+  (response-builder/build-response 200 {}))
 
 (defn router [directory parsed-request headers & body]
   (let [action (parsed-request :action)
@@ -182,4 +182,4 @@
       "PUT" (put-route  (first body) location directory)
       "DELETE" (delete-route location directory)
       "HEAD" (head-route location directory)
-      (build-response :500 {}))))
+      (response-builder/build-response 400 {}))))
