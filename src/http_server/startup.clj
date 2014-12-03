d0c7d90c79ba8e54c8cd8dc2715efad2bcff3a69
Jake Nations <jnations@8thlight.com>
2014-12-03 09:54:35 -0600

    Fix ns in require block

--- src/http_server/startup.clj
+++ src/http_server/startup.clj
@@ -1,7 +1,7 @@
 (ns http-server.startup
   (:require [http-server.cli-options :refer [parse]]
             [http-server.server :as server]
-            [http_server.handlers :as handlers-helper])
+            [http-server.handlers :as handlers-helper])
   (:gen-class))
 
 (def directory "./public")
